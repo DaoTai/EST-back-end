e63add8037702e2132fcab38f217222dad665e75
Dao Tai <ductaidao24120001@gmail.com>
2024-01-01 20:11:56 +0700

    update

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,6 +4,7 @@ import { WHITE_LIST_DOMAINS } from "~/utils/constants";
 
 export const corsOptions = {
   origin: function (origin, callback) {
+    console.log("origin: ", origin);
     // Trong môi trường dev: postman, front-end server side
     if (env.BUILD_MODE === "dev") {
       return callback(null, true);
@@ -13,7 +14,6 @@ export const corsOptions = {
     if (WHITE_LIST_DOMAINS.includes(origin)) {
       return callback(null, true);
     }
-    return callback(null, true);
     // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
     return callback(
       new ApiError({
