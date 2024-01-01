9d9af6b4f31754149ba1decf10204adcee936045
Dao Tai <ductaidao24120001@gmail.com>
2024-01-01 19:42:48 +0700

    update

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import env from "../utils/environment";
 const connectDB = () => {
   try {
-    console.log("MongoURI: ", env.MONGODB_URI);
     mongoose.connect(env.MONGODB_URI, {
       dbName: "est-learn",
       useNewUrlParser: "true",
       useUnifiedTopology: "true",
     });
+    console.log("URI Mongo: ", env.MONGODB_URI);
     console.log("Connect to DB successfully!!");
   } catch (err) {
     throw new Error("Connect to DB failed");
